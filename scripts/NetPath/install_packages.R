6c45fbfa0d7d73fc0a6fdcdcabd6cb7e02300fd3
anand-imcm <anand.maurya@well.ox.ac.uk>
2025-05-28 10:07:30 +0100

    resolving the dependencies for the new code

--- scripts/NetPath/install_packages.R
+++ scripts/NetPath/install_packages.R
@@ -1,7 +1,7 @@
-options(repos = c(CRAN = "https://cran.r-project.org"))
+options(repos = BiocManager::repositories())
 
 required_cran_packages <- c("BiocManager", "optparse", "tidyverse", "magrittr", "igraph", "fields", "ggplot2", "writexl", "pdftools")
-required_bioconductor_packages <- c("STRINGdb", "biomaRt")
+required_bioconductor_packages <- c("STRINGdb", "org.Hs.eg.db", "AnnotationDbi")
 
 missing_cran_packages <- required_cran_packages[!(required_cran_packages %in% installed.packages()[, "Package"])]
 missing_bioconductor_packages <- required_bioconductor_packages[!(required_bioconductor_packages %in% installed.packages()[, "Package"])]
@@ -10,9 +10,8 @@ if (length(missing_cran_packages)) {
     install.packages(missing_cran_packages, dependencies = TRUE)
 }
 
+BiocManager::install(version = "3.21")
+
 if (length(missing_bioconductor_packages)) {
     BiocManager::install(missing_bioconductor_packages, dependencies = TRUE)
 }
-
-ensembl <- biomaRt::useMart("ensembl", dataset = "hsapiens_gene_ensembl")
-saveRDS(file="/scripts/ensembl.RDS", object=ensembl)
