27ddccc754f510f669569a81526bacdba612a211
YUN DENG <53471532+dengyun-git@users.noreply.github.com>
2025-07-06 09:22:20 +0100

    Update protein_network_record_keep2.R

--- protein_network_record_keep2.R
+++ protein_network_record_keep2.R
@@ -1,3 +1,4 @@
+# Keep this as a record only. 
 # This script is a module for performing protein-protein interaction (PPI) network analysis on proteins selected based on protein importance derived from classification or regression machine learning models.
 
 # Example Command line for case: 
@@ -298,7 +299,7 @@ names(ProImportance) <- ModelName
 
 for(fCt in 1:length(proteinImportanceFile_list)){
   ProImportance_temp <- read.csv(paste0(proteinImportanceFile_list[fCt]), row.names=1)
-  ProImportance_temp %<>% mutate(SHAP = rowMeans(across(everything(), abs))) %<>% arrange(desc(SHAP)) %<>% dplyr::select("SHAP") 
+  ProImportance_temp %<>% mutate(SHAP = rowMeans(across(everything(), abs))) %<>% arrange(desc(SHAP)) %<>% dplyr::select("SHAP") ### Take negative SHAP into consideration
   ProImportance[[fCt]] <- ProImportance_temp
 }
 
